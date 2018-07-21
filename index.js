fd331f1f11c2cabb89ba365549a5d9b960d0dd61
Liam <liliwang@cs.stonybrook.edu>
2018-07-21 15:20:27 -0400

    Scales

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ d3.selectAll('p').style('color', 'blue');
 //    .text('D3 is awesome!!');
 //    .text(function(d) {return d;});
 
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+//var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var dataset = [1, 2, 3, 4, 5]
 
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
@@ -30,15 +31,19 @@ var svg = d3.select('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
 
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight - 30]);
+
 var barChart = svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
     .attr('y', function(d) {
-        return svgHeight - d;
+        return svgHeight - yScale(d);
     })
     .attr('height', function(d) {
-        return d;
+        return yScale(d);
     })
     .attr('width', barWidth - barPadding)
     .attr('transform', function(d, i) {
@@ -61,7 +66,7 @@ var text = svg.selectAll('text')
         return d;
     })
     .attr('y', function(d, i) {
-        return svgHeight - d - 2;
+        return svgHeight - yScale(d) - 2;
     })
     .attr('x', function(d, i) {
         return barWidth * i;
